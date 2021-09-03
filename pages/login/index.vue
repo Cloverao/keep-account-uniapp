d5e55e5d8a3b9729e8a4e32ed7987c9201a28c1e
huangpiao <huangpiao35@zto.com>
2021-09-03 14:24:25 +0800

    init project

---
+++ pages/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="content"> </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello",
+    };
+  },
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style></style>
